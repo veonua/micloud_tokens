075f90ca91ba06faa585e8259f2486f172a6be18
Andrew <veon.ua@gmail.com>
2022-08-23 22:31:12 +0200

    first version

---
+++ main.py
@@ -0,0 +1,44 @@
+import micloud
+import streamlit as st
+
+username = st.text_input("Enter your name")
+password = st.text_input("Enter your password", type="password")
+server = st.selectbox("Select server", ["cn", "de", "i2", "ru", "sg", "us"])
+
+products = {
+    577: "Mi Robot Vacuum",
+    51: "Yeelight",
+    109: "Mi Control Hub",
+    245: "AC Partner",
+    65567: "Infrared Remote",
+    206: "Toothbrush",
+    152: "Plantmonitor",
+}
+
+login = None
+if username and password:
+    cloud = micloud.MiCloud(username, password)
+    cloud.default_server = server
+    login = cloud.login()
+
+if login:
+    devices = cloud.get_devices()
+
+    short_devices = [
+        {'name':dev.get("name"),
+         'localip':dev.get("localip"),
+         'token':dev.get("token"),
+         'pd_id': dev.get("pd_id"),  # product id see products
+         'model':dev.get("model"),
+         'ssid':dev.get("ssid"),
+         } for dev in devices if dev.get("token")]
+
+    my_dict = {}
+    for v in short_devices:
+        k = v.pop('ssid')
+        my_dict.setdefault(k, []).append(v)
+
+    st.write(my_dict)
+else:
+    st.write("Login failed")
+
